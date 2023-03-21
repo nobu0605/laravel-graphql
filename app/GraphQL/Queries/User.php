1b24d21e22e061a2dc3beb78fed4f0a0eed2ce96
Takanobu Suzuki <nobublack555@gmail.com>
2023-03-21 19:02:32 +0900

    add custom resolver

---
+++ app/GraphQL/Queries/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\User as UserModel;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+final class User
+{
+    public function getUserById($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $user = UserModel::where('id',$args["id"])->first();
+        
+        return $user;
+    }
+}
+
+
